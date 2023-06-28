ed0932525c9ffc14fc44d8204821d4e9ba3fbac5
Eudes89 <eudesmith@gmail.com>
2023-06-28 20:23:00 -0300

    0

--- app/page.js
+++ app/page.js
@@ -13,12 +13,14 @@ export default function Home() {
     overscroll-x-none overflow-x-hidden">
       
       
-      <iframe src="https://player.vimeo.com/video/840283518?h=0be9a2311a" width="640" height="564"
-      seamless frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>  
-     
-      <iframe 
-      src="https://player.vimeo.com/video/840283461?h=f8c1667088" width="640" height="564" 
-      seamless frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      <div style="padding:56.25% 0 0 0;position:relative;">
+        <iframe src="https://player.vimeo.com/video/840283461?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        frameborder="0" allow="autoplay; fullscreen; picture-in-picture; muted"
+        allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        title="earth-pc">
+          </iframe>
+      </div>
+      
       {/* <video 
         src="https://vimeo.com/840283518?share=copy"
         autoPlay loop muted
