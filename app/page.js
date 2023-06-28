3276f89823d72539c79bdb166daf52d8fb3f32e2
Eudes89 <eudesmith@gmail.com>
2023-06-27 22:00:44 -0300

    vimeo videos background

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 
 import ProfilePhoto from "@/components/profilePhoto";
-import bgMobile from '../public/videos/sky-mobile.mp4';
+// import bgMobile from '../public/videos/sky-mobile.mp4';
 import Menu from "@/components/menu";
 import bgPc from '../public/videos/earth-pc.mp4';
 
@@ -12,16 +12,38 @@ export default function Home() {
     <main className="flex flex-col items-center h-screen
     overscroll-x-none overflow-x-hidden">
       
-      {/* background video MOBILE */}
-      <video 
+      <iframe 
+      src="https://player.vimeo.com/video/840283461?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+      width="3840" height="2160" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" 
+      allowfullscreen title="earth-pc" className="fixed min-w-[600px] min-h-screen
+      w-auto h-auto -z-10
+      md:hidden lg:hidden xl:hidden">
+
+      </iframe>
+        
+     
+      
+      {/* <video 
         src={bgMobile}
         autoPlay loop muted
         className="fixed min-w-[600px] min-h-screen
         w-auto h-auto -z-10
         md:hidden lg:hidden xl:hidden"
-      />
+      /> */}
 
-      <video
+      <iframe 
+      src="https://player.vimeo.com/video/840283518?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+      width="1440" height="2160" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"
+      allowfullscreen title="sky-mobile" className="hidden
+      2xl:flex xl:flex lg:flex md:flex
+      fixed min-w-[1840px] min-h-[960px] w-auto h-auto
+      -z-10 2xl:min-w-[3840px] 2xl:min-h-[2160]
+      3xl:min-w-[3840px] 3xl:min-h-[2160]
+      xl:min-w-[2240px] xl:min-h-[1600]">
+      
+      </iframe>
+
+      {/* <video
         src={bgPc}
         autoPlay loop muted
         className="hidden
@@ -31,7 +53,7 @@ export default function Home() {
         3xl:min-w-[3840px] 3xl:min-h-[2160]
         xl:min-w-[2240px] xl:min-h-[1600]"
       
-      />
+      /> */}
       
       <ProfilePhoto>
 
