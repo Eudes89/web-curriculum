e97aa85aa4491d7e6a3c270ce53ba58ae2460d46
Eudes89 <eudesmith@gmail.com>
2023-06-28 20:30:28 -0300

    try again

--- app/page.js
+++ app/page.js
@@ -13,10 +13,10 @@ export default function Home() {
     overscroll-x-none overflow-x-hidden">
       
       
-      <div style="padding:56.25% 0 0 0;position:relative;">
+      <div >
         <iframe src="https://player.vimeo.com/video/840283461?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
         frameborder="0" allow="autoplay; loop; muted"
-        allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        allowfullscreen 
         title="earth-pc">
           </iframe>
       </div>
