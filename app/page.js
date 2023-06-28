4db45b05a3eec796ed4e014ead86792904eb3c2e
Eudes89 <eudesmith@gmail.com>
2023-06-28 20:33:49 -0300

    try again 2

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <div >
         <iframe src="https://player.vimeo.com/video/840283461?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
         frameborder="0" allow="autoplay; loop; muted"
-        allowfullscreen 
+        className="fixed -z-10 min-h-screen min-w-screen"
         title="earth-pc">
           </iframe>
       </div>
