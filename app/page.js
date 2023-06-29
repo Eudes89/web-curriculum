4804d6f0355d638b099b9f97f2bcbe53db815e0f
Eudes89 <eudesmith@gmail.com>
2023-06-28 21:31:28 -0300

    value

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 
 import ProfilePhoto from "@/components/profilePhoto";
-// import bgMobile from '../public/videos/sky-mobile.mp4';
+import bgMobile from '../public/videos/sky-mobile.mp4';
 import Menu from "@/components/menu";
 // import bgPc from '../public/videos/earth-pc.mp4';
 
@@ -13,31 +13,14 @@ export default function Home() {
     overscroll-x-none overflow-x-hidden">
       
       
-      <div className="w-auto h-auto flex justify-center items-center" >
-      <iframe width="3840" height="2160" src="https://www.youtube.com/embed/FTMHpSgLMa0?autoplay=1&controls=0&loop=1&modestbranding=1" 
-      title="YouTube video player" frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; loop" 
-      allowfullscreen
-      className="fixed 2xl:min-w-[3840px] 2xl:min-h-[2160px]
-      xl:min-w-[1980px] xl:min-h-[1080px]
-      lg:min-w-[1260px] lg:min-h-[720px]
-      md:min-w-[980px] md:min-h-[600px]">
-
-      </iframe>
-          
-      </div>
-      
-      {/* <video 
-        src="https://vimeo.com/840283518?share=copy"
+      <video 
+        src={bgMobile}
         autoPlay loop muted
         className="fixed min-w-[600px] min-h-screen
         w-auto h-auto -z-10
         md:hidden lg:hidden xl:hidden"
-      /> */}
-
+      />
       
-
-      {/*  */}
       
       <ProfilePhoto>
 
